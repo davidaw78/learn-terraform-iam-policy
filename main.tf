d969d4ff9e9e098ab5ea960663742da77e16a55b
David <foabdavid@gmail.com>
2023-12-29 07:33:54 +0000

    Learn how to add aws_iam_user to aws_iam_group

--- main.tf
+++ main.tf
@@ -15,15 +15,27 @@ resource "aws_iam_user" "dev_user" {
   name = "dev-user"
 }
 
+resource "aws_iam_group" "dev_group" {
+  name = "dev-group"
+}
+
+resource "aws_iam_user_group_membership" "dev_group_membership" {
+  user = aws_iam_user.dev_user.name
+  groups = [
+    aws_iam_group.dev_group.name
+  ]
+}
+
 resource "aws_iam_user_login_profile" "dev_user_login_profile" {
  user    = aws_iam_user.dev_user.name
- pgp_key = "keybase:dev-user"
+# password               = "YourCustomPassword123"  # Set your custom password here
+# pgp_key = "keybase:dev-user"
 }
 
 # testing staging branch
-#output "password" {
-# value = aws_iam_user_login_profile.dev_user_login_profile.password
-#}
+output "password" {
+ value = aws_iam_user_login_profile.dev_user_login_profile.password
+}
 
 resource "aws_iam_policy" "policy" {
   name        = "test_policy"
